82870a0865f6f1310ee0867079b6ab792ec459dd
mariapetriti <petriti.maria@gmail.com>
2018-02-20 17:20:52 +0200

    connection to db

---
+++ db-config-template.js
@@ -0,0 +1,7 @@
+export const DB_CONF = {
+    user: 'db_user',
+    host: 'your.domain.com',
+    database: 'db',
+    password: 'secretpassword',
+    port: 1234
+}
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+const { Client } = require('pg');
+const  DB_CONF  = require('./db-config')
+const client = new Client(DB_CONF);
 const express = require('express');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use("/", express.static(__dirname + '/public'));
 
-app.get('/api/test', (req, res) => res.send('Data visualisation with D3'));
+app.get('/api/aggregated', (req, res) => {
+    client.connect();
+    client.query('Select * from aggregated_data', (err, result) => {
+        client.end();
+        if (err) {
+            res.send('Error retrieving data');
+        }
+        else {
+            res.send(result.rows);
+        }
+    });
+});
 
 app.listen(app.get('port'), () => console.log('App is listening to 5000'));
