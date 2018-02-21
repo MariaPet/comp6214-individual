d67704a8c6e24a54e49cae2aa28e00b23d54f1b8
mariapetriti <petriti.maria@gmail.com>
2018-02-21 20:29:03 +0200

    define appropriate axis intercection

--- public/js/d3Init.js
+++ public/js/d3Init.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
             console.log(error);
         }
 
-        var width = 800;
-        var height = 800;
+        var width = 1600;
+        var height = 600;
         var margin = {
             top: 30,
             right: 30,
@@ -28,25 +28,36 @@ $(document).ready(function() {
         .attr('height', height + margin.top + margin.bottom).append("g")
         .attr('transform', 'translate('+margin.left+','+margin.top+')');
 
-        var x = d3.scaleLinear().domain([-100, 100]).range([0, width]);
-        var y = d3.scaleLinear().domain([-100, 100]).range([height, 0]);
-        var point = svg.selectAll(".point").data(dataset)
-        .enter().append("circle").attr("class", "point").attr("cy", function(d) {
+        var x = d3.scaleLinear().domain([-55, 10]).range([0, width]);
+        var y = d3.scaleLinear().domain([-50, 15]).range([height, 0]);
+        var point = svg.selectAll(".point").data(dataset).enter();
+        point.append("circle").attr("class", "point")
+        .attr("cy", function(d) {
             return y(d["Avg. Cost Variance(%)"]);
         }).attr("cx", function(d) {
             return x(d["Avg. Scedule Variance(%)"]);
         }).attr("r", function(d) {
-            return d["Avg. Project Cost ($ M)"];
+            return Math.sqrt(d["Avg. Project Cost ($ M)"]/Math.PI)*10;
         }).attr("stroke", function(d) {
             return colour(d["Projects Number"]);
         }).attr("fill", function(d) {
             return colour(d["Projects Number"]);
-        }).attr("opacity", 0.8);
-
-        svg.append("g").attr("transform", "translate(0, "+ (height/2)+")")
+        }).attr("opacity", 0.7)
+        // point.append("text").text(function(d) {
+        //     return d["Agency Name"];
+        // }).attr("dx", function(d) {
+        //     return x(d["Avg. Scedule Variance(%)"]);
+        // }).attr("dy", function(d) {
+        //     return y(d["Avg. Cost Variance(%)"]);
+        // })
+        // .attr("text-anchor", "middle")
+        // .attr('transform', function(d) {
+        //     return 'translate('+ (-d["Avg. Project Cost ($ M)"]/2) +', 0)'
+        // });
+        svg.append("g").attr("transform", "translate(0, "+ y(0) +")")
         .call(d3.axisBottom(x));
 
-        svg.append("g").attr("transform", "translate("+ (width/2) +", 0)").call(d3.axisLeft(y));//.attr("transform", "translate("+ (width)/2 +", 0)")
+        svg.append("g").attr("transform", "translate("+ x(0) +", 0)").call(d3.axisLeft(y));//.attr("transform", "translate("+ (width)/2 +", 0)")
 
     });    
  
